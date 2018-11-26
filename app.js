247d3195853322a0fd6062863487a3936798621b
Erik Sundell <erik.sundell87@gmail.com>
2018-11-26 23:25:10 +0100

    oops

---
+++ app.js
@@ -0,0 +1,18 @@
+var heroku = require('heroku-ping');
+
+heroku.ping({
+  interval: 1000 * 60 * 15,
+  silent: false,
+  apps: [
+    {
+      name: 'tennistider',
+      path: '/',
+      secure: true
+    },
+    {
+      name: 'tennistider-api',
+      path: '/health',
+      secure: true
+    }
+  ]
+});
