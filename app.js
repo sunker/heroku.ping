955af46b6e1df22a4931d2a4b9a4c9b0ed6d1328
Erik Sundell <erik.sundell87@gmail.com>
2019-11-18 22:22:24 +0100

    Add service

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var heroku = require('heroku-ping');
+var heroku = require("heroku-ping");
 
 heroku.ping({
   interval: 1000 * 60 * 15,
   silent: false,
   apps: [
     {
-      name: 'tennistider',
-      path: '/',
+      name: "tennistider",
+      path: "/",
       secure: true
     },
     {
-      name: 'tennistider-api',
-      path: '/health',
+      name: "tennistider-api",
+      path: "/health",
+      secure: true
+    },
+    {
+      name: "tennistider-scraper",
+      path: "/health",
       secure: true
     }
   ]
